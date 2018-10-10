6d2d6c7a9aafa9fba1deebabf65f8e75301a02ed
Astrid Deschenes <adeschen@hotmail.com>
2018-10-09 20:02:33 -0400

    Formating code

--- R/smedian.sample.R
+++ R/smedian.sample.R
@@ -15,9 +15,8 @@
 #' @author Alexander Krasnitz, Guoli Sun
 #' @importFrom stats median
 #' @keywords internal
-smedian.sample <-
-function(pos, v)
+smedian.sample <- function(pos, v)
 {
-	w<-v[pos[1]:pos[2]][!is.na(v[pos[1]:pos[2]])]
-	return(median(sample(w,length(w),replace=T),na.rm=T))
+	w <- v[pos[1]:pos[2]][!is.na(v[pos[1]:pos[2]])]
+	return(median(sample(w, length(w), replace=T), na.rm=T))
 }
