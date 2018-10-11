29726b4e7bed7702f5149c703322a9bf1d3371e4
Astrid Deschenes <adeschen@hotmail.com>
2018-10-10 20:05:16 -0400

    Adding unit test for makeCNPmask() function

---
+++ tests/testthat/test-makeCNPmask.R
@@ -0,0 +1,33 @@
+### Unit tests for makeCNPmask function
+
+library(CNprep)
+
+
+
+### Tests makeCNPmask() results
+
+context("makeCNPmask() results")
+
+test_that("makeCNPmask() must return expected results 01", {
+    
+    ampTemp01 <- data.frame(copy.num=c(rep("amp", 6)), 
+                                 chrom=c(1, 2,  1,  5,  1, 3),
+                                 chrom.start=c(12842827, 86198238, 13071912, 233492, 13071912, 199030879),
+                                 chrom.end=c(13439275, 86575258, 13439275, 292937, 13456522, 199326099), stringsAsFactors = FALSE)
+    
+    RNGkind("default")
+    
+    set.seed(211)
+    
+    results <- makeCNPmask(imat=ampTemp01, chromcol="chrom", 
+                           startcol="chrom.start", endcol="chrom.end",
+                           nprof=3, uthresh=0.02, dthresh=0.008)
+    
+    expected <- matrix(c(1, 2, 3, 5, 
+                         12842827, 86198238, 199030879, 233492, 
+                         13456522, 86575258, 199326099, 292937), 
+                       ncol = 3, byrow = FALSE)
+    colnames(expected) <- c("chrom", "start", "end")
+    
+    expect_equal(results, expected)
+})
