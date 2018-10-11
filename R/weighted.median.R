1c0db137a0dd466c052464aa4cdce6ad426e15db
Astrid Deschenes <adeschen@hotmail.com>
2018-10-10 21:03:10 -0400

    Adding man page and unit tests for weighted.median() function

--- R/weighted.median.R
+++ R/weighted.median.R
@@ -1,5 +1,31 @@
-weighted.median <-
-function(v,weights){
+#' @title Calculate the weighted median of a specified vector.
+#' 
+#' @description Compute the weighted median of a specified vector.
+#' 
+#' @param v a \code{vector} of \code{double} containing the values used for
+#' the calculation.
+#' 
+#' @param weights a \code{vector} of \code{double} containing the weight for 
+#' each value of the \code{v} vector.
+#' 
+#' @return a \code{double} which is the weighted median of the vector.
+#' 
+#' @examples
+#' 
+#' ## Vector of values
+#' values <- c(0.06363411, 0.04342896, 0.07207384, 0.07319237, 0.07273546,
+#'     0.01463932, 0.02136043, 0.01967027)
+#' 
+#' ## Vector of weigth associated to each value    
+#' weight <- c(1, 1, 1, 1, 0, 0, 0, 0)
+#' 
+#' ## Calculation of the weighted median
+#' CNprep:::weighted.median(v=values, weights=weight)
+#' 
+#' @author Alexander Krasnitz, Guoli Sun
+#' @keywords internal
+weighted.median <- function(v, weights)
+{
 	weights<-weights[order(v)]
 	v<-sort(v)
 	sw<-sum(weights)
