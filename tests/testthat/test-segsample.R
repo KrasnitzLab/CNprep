fcfaf66eafca1c8e79ee78e015489223d83f6076
Astrid Deschenes <adeschen@hotmail.com>
2018-10-15 19:54:03 -0400

    Adding unit tests for segsample() function

--- tests/testthat/test-segsample.R
+++ tests/testthat/test-segsample.R
@@ -7,7 +7,7 @@ library(CNprep)
 
 context("segsample() results")
 
-test_that("segsample() must return expected results", {
+test_that("segsample() must return expected results 01", {
     
     segData <- data.frame(StartProbe=c(1, 7, 13),
                           EndProbe=c(6, 12, 15),
@@ -31,6 +31,65 @@ test_that("segsample() must return expected results", {
     expected <- data.frame(StartProbe=c(1, 7),
                            EndProbe=c(6, 12),
                            results=c(0.09305272500, 0.08662475200))
+    
+    expect_equal(results, expected)
+})
+
+test_that("segsample() must return expected results 02", {
+    
+    segData <- data.frame(StartProbe=c(1, 9, 14),
+                          EndProbe=c(8, 13, 15),
+                          chrom=c(1,1,1),
+                          segmedian=c(0.08662475, 0.07319237, 0.07689544),
+                          segmad=c(0.06404208, 0.04764233, 0.08833202))
+    ratcol <- c(0.066076840, 0.119916919,  0.124456689,  0.046694620, -0.077843732, 
+                0.011052725,  0.178808930,  0.122289490,  0.086624752, -0.003855011, 
+                -0.195791649, 0.063634112,  0.111449474,  0.043428961,  0.160174529)
+    
+    
+    RNGkind("default")
+    
+    set.seed(2222)
+    
+    results <- CNprep:::segsample(mysegs=segData, ratcol=ratcol, startcol="StartProbe", endcol="EndProbe",
+                                  blocksize = 4)
+    
+    colnames(results)[3] <- "results"
+    
+    expected <- data.frame(StartProbe=c(1, 1, 9),
+                           EndProbe=c(8, 8, 13),
+                           results=c(0.1211032045000, 0.0466946200000, 0.1114494740000))
+    row.names(expected) <- c("1",   "1.1", "2")
+    
+    expect_equal(results, expected)
+})
+
+test_that("segsample() must return expected results when using parameter times", {
+    
+    segData <- data.frame(StartProbe=c(1, 9, 14),
+                          EndProbe=c(8, 13, 15),
+                          chrom=c(1,1,1),
+                          segmedian=c(0.08662475, 0.07319237, 0.07689544),
+                          segmad=c(0.06404208, 0.04764233, 0.08833202))
+    ratcol <- c(0.066076840, 0.119916919,  0.124456689,  0.046694620, -0.077843732, 
+                0.011052725,  0.178808930,  0.122289490,  0.086624752, -0.003855011, 
+                -0.195791649, 0.063634112,  0.111449474,  0.043428961,  0.160174529)
+    
+    
+    RNGkind("default")
+    
+    set.seed(222)
+    
+    results <- CNprep:::segsample(mysegs=segData, ratcol=ratcol, startcol="StartProbe", endcol="EndProbe",
+                                    times=2)
+    
+    colnames(results)[3] <- "results"
+    
+    expected <- data.frame(StartProbe=c(1, 1, 9, 9, 14, 14),
+                           EndProbe=c(8, 8, 13 , 13, 15, 15),
+                           results=c(0.094183165000000, 0.066076840000000, 0.086624752000000,
+                                     -0.003855011000000, 0.101801745000000, 0.101801745000000))
+    row.names(expected) <- c("1",   "1.1", "2", "2.1", "3", "3.1")
     
     expect_equal(results, expected)
 })
