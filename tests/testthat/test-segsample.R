c07ccddf484bbcf910ecf84f31775b36247ab1d0
Astrid Deschenes <adeschen@hotmail.com>
2018-10-15 20:03:13 -0400

    Adding one unit test for segsample() function

--- tests/testthat/test-segsample.R
+++ tests/testthat/test-segsample.R
@@ -64,7 +64,7 @@ test_that("segsample() must return expected results 02", {
     expect_equal(results, expected)
 })
 
-test_that("segsample() must return expected results when using parameter times", {
+test_that("segsample() must return expected results when using parameter times 01", {
     
     segData <- data.frame(StartProbe=c(1, 9, 14),
                           EndProbe=c(8, 13, 15),
@@ -90,6 +90,37 @@ test_that("segsample() must return expected results when using parameter times",
                            results=c(0.094183165000000, 0.066076840000000, 0.086624752000000,
                                      -0.003855011000000, 0.101801745000000, 0.101801745000000))
     row.names(expected) <- c("1",   "1.1", "2", "2.1", "3", "3.1")
+    
+    expect_equal(results, expected)
+})
+
+test_that("segsample() must return expected results when using parameter times 02", {
+    
+    segData <- data.frame(StartProbe=c(1, 7, 13),
+                          EndProbe=c(6, 12, 15),
+                          chrom=c(1,1,1),
+                          segmedian=c(0.08662475, 0.07319237, 0.07689544),
+                          segmad=c(0.06404208, 0.04764233, 0.08833202))
+    ratcol <- c(0.072073840, 0.119913919,  0.154459489,  0.040994620, -0.082843732, 
+                0.093052725,  0.170908930,  0.100289490,  0.086624752, -0.003855011, 
+                -0.195791649, 0.063634112,  0.109449474,  0.043428961,  0.160174529)
+    
+    
+    RNGkind("default")
+    
+    set.seed(23322)
+    
+    results <- CNprep:::segsample(mysegs=segData, ratcol=ratcol, startcol="StartProbe", endcol="EndProbe",
+                                    times=3)
+    
+    colnames(results)[3] <- "results"
+    
+    expected <- data.frame(StartProbe=c(1, 1, 1, 7, 7, 7, 13, 13, 13),
+                           EndProbe=c(6, 6, 6, 12, 12, 12, 15, 15, 15),
+                           results=c(0.09305272500, 0.13718670400, 0.10648332200, 0.02988955050,
+                                     0.08196180100, 0.07512943200, 0.10944947400, 0.16017452900,
+                                     0.10944947400))
+    row.names(expected) <- c("1", "1.1", "1.2", "2", "2.1", "2.2", "3", "3.1", "3.2")
     
     expect_equal(results, expected)
 })
