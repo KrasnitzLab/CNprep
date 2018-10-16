c62fd6414ce7916d2805784118603e9ac3cd4e9b
Astrid Deschenes <adeschen@hotmail.com>
2018-10-16 13:54:43 -0400

    Formatting code of segsample() function

--- R/segsample.R
+++ R/segsample.R
@@ -69,8 +69,8 @@
 segsample <- function(mysegs, ratcol, startcol="StartProbe", endcol="EndProbe",
                         blocksize=0, times=0)
 {
-    if(blocksize==0&times==0)stop("One of blocksize or times must be set")
-    if(blocksize!=0&times!=0)stop("Only one of blocksize or times can be set")
+    if(blocksize == 0 & times == 0) stop("One of blocksize or times must be set")
+    if(blocksize != 0 & times != 0) stop("Only one of blocksize or times can be set")
     segtable <- mysegs[,c(startcol,endcol), drop=FALSE]
     ## Comment Pascal: at least one result should be different from zero
     if (blocksize != 0) segtable<-
