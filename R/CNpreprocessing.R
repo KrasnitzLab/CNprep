b49ac111515192898e4cd72edd7eeb20d6eaad03
Astrid Deschenes <adeschen@hotmail.com>
2019-11-06 18:01:27 -0500

    Formatting to respect bioconductor length of lines and space usage

--- R/CNpreprocessing.R
+++ R/CNpreprocessing.R
@@ -236,35 +236,59 @@ CNpreprocessing <- function(segall, ratall=NULL, idcol=NULL, startcol=NULL,
     if(is.null(idcol)){
         cat("Found a single segmented profile with no ID","\n")
         if(!is.null(ratall)){
-            if(sum(apply(ratall,2,data.class)=="numeric")>1)
-                stop("Ambiguity: more than 1 numeric column in raw data table\n")
-            else {
-                idrat <- which(apply(ratall,2,data.class)=="numeric")
-                segall <- data.frame(rep(as.character(idrat),nrow(segall)), segall)
+            if (sum(apply(ratall, 2, data.class) == "numeric") > 1) {
+                stop(paste0("Ambiguity: more than 1 numeric column in raw ", 
+                                "data table\n"))
+            } else {
+                idrat <- which(apply(ratall, 2, data.class) == "numeric")
+                segall <- data.frame(rep(as.character(idrat), nrow(segall)), 
+                                        segall)
                 idcol <- "ID"
                 dimnames(segall)[[2]][1] <- idcol
             }
         }
     }
 
-    if(is.null(ratall))cat("No raw table, proceeding to comparison\n")
-    else {
+    if (is.null(ratall)) {
+        cat("No raw table, proceeding to comparison\n")
+    } else {
         profnames <- unique(segall[,idcol])
-        if(!all(profnames%in%dimnames(ratall)[[2]]))
+        
+        if (!all(profnames %in% dimnames(ratall)[[2]])) {
             stop("Found unmatched segmented profile IDs\n")
-        if(is.null(startcol)|is.null(endcol)){ #will need an annotation table
-            if(is.null(bpstartcol)|is.null(bpendcol)|is.null(chromcol))
+        }
+        
+        if (is.null(startcol) | is.null(endcol)) { 
+            
+            # Will need an annotation table
+            
+            if (is.null(bpstartcol) | is.null(bpendcol) | is.null(chromcol)) {
                 stop("Unable to proceed: incomplete segment annotation\n")
-            if(is.null(chromrange))chromrange<-sort(unique(segall[,chromcol]))
-            if(is.null(annot))
-                stop("No annotation table; unable to determine boundary probes/bins\n")
-            if(is.null(annotstartcol)|is.null(annotchromcol))
-                stop("No start and chrom column names provided for annotation table\n")
-            if(useend&is.null(annotendcol))
-                stop("End column name required but not provided in annotation table\n")
-            maxbpstart <- max(c(segall[,bpstartcol], annot[,annotstartcol]))+1
-            maxbpend <- ifelse(useend, max(c(segall[,bpendcol], annot[,annotendcol])),
-                                max(c(segall[,bpendcol],annot[,annotstartcol])))+1
+            }
+            
+            if (is.null(chromrange)) {
+                chromrange <- sort(unique(segall[,chromcol]))
+            }
+            
+            if (is.null(annot)) {
+                stop(paste0("No annotation table; unable to determine ", 
+                                "boundary probes/bins\n"))
+            }
+            
+            if (is.null(annotstartcol) | is.null(annotchromcol)) {
+                stop(paste0("No start and chrom column names provided for ", 
+                                "annotation table\n"))
+            }
+            
+            if (useend & is.null(annotendcol)) {
+                stop(paste0("End column name required but not provided in ", 
+                            "annotation table\n"))
+            }
+            
+            maxbpstart <- max(c(segall[,bpstartcol], annot[,annotstartcol])) + 1
+            maxbpend <- ifelse(useend, 
+                        max(c(segall[,bpendcol], annot[,annotendcol])),
+                        max(c(segall[,bpendcol], annot[,annotstartcol]))) + 1
             startprobe <- match((segall[,chromcol]-1)*maxbpstart+segall[,bpstartcol],
                                 ceiling((annot[,annotchromcol]-1)*maxbpstart+annot[,annotstartcol]))
             endprobe <- ifelse(rep(useend,length(startprobe)),
@@ -272,29 +296,37 @@ CNpreprocessing <- function(segall, ratall=NULL, idcol=NULL, startcol=NULL,
                                 ceiling((annot[,annotchromcol]-1)*maxbpend+annot[,annotendcol])),
                             match((segall[,chromcol]-1)*maxbpend+segall[,bpendcol],
                                 ceiling((annot[,annotchromcol]-1)*maxbpend+annot[,annotstartcol])))
-            if(!all(!is.na(startprobe)&!is.na(endprobe)))
+            
+            if (!all(!is.na(startprobe) & !is.na(endprobe))) {
                 stop("Incomplete start and end annotation of segments\n")
-            segall <- data.frame(segall,startprobe,endprobe)
-            dimnames(segall)[[2]][(ncol(segall)-1):ncol(segall)] <- c("StartProbe", "EndProbe")
+            }
+            
+            segall <- data.frame(segall, startprobe, endprobe)
+            dimnames(segall)[[2]][(ncol(segall)-1):ncol(segall)] <- 
+                                                    c("StartProbe", "EndProbe")
             startcol <- "StartProbe"
             endcol <- "EndProbe"
         }
-        profpack<-vector(mode="list",length=length(profnames))
-        names(profpack)<-profnames
-        for(pn in profnames){
-            profpack[[pn]] <- vector(mode="list",length=4)
-            names(profpack[[pn]]) <- c("seg","rat","stream","sub")
-            profpack[[pn]]$seg<-
-                segall[segall[,idcol]==pn,c(startcol,endcol,chromcol), drop=FALSE]
+        
+        profpack <- vector(mode="list", length = length(profnames))
+        names(profpack) <- profnames
+        
+        for (pn in profnames) {
+            profpack[[pn]] <- vector(mode="list", length=4)
+            names(profpack[[pn]]) <- c("seg", "rat", "stream", "sub")
+            profpack[[pn]]$seg <-
+                segall[segall[,idcol] == pn, c(startcol, endcol, chromcol), 
+                        drop=FALSE]
             dimnames(profpack[[pn]]$seg)[[2]] <- c("StartProbe", 
                                                     "EndProbe", "chrom")
             profpack[[pn]]$rat <- ratall[,pn]
             profpack[[pn]]$stream <- pn
-            profpack[[pn]]$sub <- match(pn,profnames)
+            profpack[[pn]]$sub <- match(pn, profnames)
         }
+        
         rm(ratall)
         gc()
-        distrib<-match.arg(distrib)
+        distrib <- match.arg(distrib)
 
         if (distrib=="Rparallel") {
             ncores <- min(njobs,length(profnames),detectCores())
@@ -314,28 +346,41 @@ CNpreprocessing <- function(segall, ratall=NULL, idcol=NULL, startcol=NULL,
                 bestbic=bestbic, modelNames=modelNames, cweight=cweight,
                 bstimes=bstimes, chromrange=chromrange, seedme=myseed))
         if (distrib=="Rparallel") stopCluster(cl)
-        segall<-cbind(segall,do.call(rbind,processed))
+        segall <- cbind(segall, do.call(rbind, processed))
         dimnames(segall)[[2]][(ncol(segall)-8):ncol(segall)] <-
             c("segmedian", "segmad", "mediandev", "segerr", "centerz",
                 "marginalprob", "maxz", "maxzmean", "maxzsigma")
-        medcol<-"mediandev"
-        madcol<-"segmad"
-        errorcol<-"segerr"
+        medcol <- "mediandev"
+        madcol <- "segmad"
+        errorcol <- "segerr"
     }
-    if (!(is.null(normalength)|is.null(normalmedian)|is.null(medcol))) {
-        if (is.null(bpstartcol)|is.null(bpendcol)) {  #try to annotate
-            if(is.null(startcol)|is.null(endcol)|is.null(annot)|is.null(annotstartcol)
-                |is.null(annotendcol))stop("Insufficient annotation for comparison")
-            tumorlength <- annot[segall[,endcol],annotendcol]-
-                annot[segall[,startcol],annotstartcol]+1
+    if (!(is.null(normalength) | is.null(normalmedian) | is.null(medcol))) {
+        if (is.null(bpstartcol) | is.null(bpendcol)) {  #try to annotate
+            if (is.null(startcol) | is.null(endcol) | is.null(annot) | 
+                is.null(annotstartcol) | is.null(annotendcol)) {
+                stop("Insufficient annotation for comparison")
+            }
+            
+            tumorlength <- annot[segall[,endcol], annotendcol] -
+                annot[segall[,startcol], annotstartcol] + 1
+        } else {
+            tumorlength<-segall[,bpendcol]-segall[,bpstartcol] + 1
         }
-        else tumorlength<-segall[,bpendcol]-segall[,bpstartcol]+1
+        
         tumormedian <- segall[,medcol]
-        if(!is.null(madcol))tumormad<-segall[,madcol]
-        if(!is.null(errorcol))tumorerror<-segall[,errorcol]
-        segall<-cbind(segall, normalComparison(normalmedian, normalength,
+        
+        if (!is.null(madcol)) {
+            tumormad<-segall[,madcol]
+        }
+        
+        if (!is.null(errorcol)) {
+            tumorerror<-segall[,errorcol]
+        }
+        
+        segall <- cbind(segall, normalComparison(normalmedian, normalength,
                     tumormedian, tumorlength, normalmad, normalerror, tumormad, 
                     tumorerror))
     }
+    
     return(segall)
 }
