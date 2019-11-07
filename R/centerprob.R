c82985a021d5f01869afa45a67442b89b6b9cd5e
Astrid Deschenes <adeschen@hotmail.com>
2019-11-07 17:45:46 -0500

    Formatting centerprob() function to respect bioconductor format

--- R/centerprob.R
+++ R/centerprob.R
@@ -2,7 +2,7 @@
 #' 
 #' @description TODO
 #' 
-#' @param logr TODO
+#' @param logr TODO a \code{vector} of \code{numeric}
 #' 
 #' @param emfit An object of class \code{Mclust} providing a 
 #' mixture model estimation. 
@@ -11,7 +11,7 @@
 #' 
 #' @param center TODO
 #' 
-#' @return TODO
+#' @return TODO a \code{vector} of \code{numeric}
 #' 
 #' @examples
 #' 
@@ -21,21 +21,37 @@
 #' @importFrom stats pnorm
 #' @keywords internal
 centerprob <- function(logr, emfit, zgroup, times, center) { 
-    gz <- matrix(nrow=length(logr), ncol=length(emfit$parameters$mean), 
+    
+    ## Create new matrix using input data
+    gz <- matrix(nrow = length(logr), ncol = length(emfit$parameters$mean), 
                     data=logr)
+    
     #gz<-t(emfit$pro*exp(-0.5*(t(gz)-emfit$mu)^2/emfit$sigma)/
     #sqrt(2*pi*emfit$sigma))
-    if(length(emfit$parameters$mean)==1) epro <- as.vector(1)
-    if(length(emfit$parameters$mean)>1) epro <- emfit$parameters$pro        
-    gz <- t(epro*pnorm(-abs(t(gz)-emfit$parameters$mean)/
-        sqrt(emfit$parameters$variance$sigmasq)))
+    
+    ## Assign correct value to epro object according to mean length
+    if (length(emfit$parameters$mean) == 1) { 
+        epro <- as.vector(1)
+    }
+    
+    if (length(emfit$parameters$mean) > 1) {
+        epro <- emfit$parameters$pro        
+    }
+    
+    gz <- t(epro * pnorm(-abs(t(gz) - emfit$parameters$mean) /
+                sqrt(emfit$parameters$variance$sigmasq)))
     
     ## Combine columns of z table using indicator matrix zgroup
-    gz <- gz%*%t(zgroup)  
+    gz <- gz %*% t(zgroup)  
     
-    gz <- matrix(ncol=ncol(gz),
-        data=apply(gz,2,cumsum)[seq(from=times,to=nrow(gz),by=times),]/times)
+    gz <- matrix(ncol = ncol(gz),
+            data = apply(gz, 2, cumsum)[seq(from = times, 
+                                    to = nrow(gz), by = times),] / times)
+    
     ## Mean value within each segment
-    gz <- (gz[,center]-c(0,gz[-nrow(gz),center]))/sum(epro[zgroup[center,]==1]) 
-    return(ifelse(gz<0.5, gz, 1-gz))
+    gz <- (gz[,center] - c(0, gz[-nrow(gz), center])) / 
+                            sum(epro[zgroup[center,] == 1])
+    
+    ## Ensure that the return value is always inferior to 0.5
+    return(ifelse(gz < 0.5, gz, 1 - gz))
 }
