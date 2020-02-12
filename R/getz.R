2f1a254de7d072bf213e9aef391361fdadfbe974
Astrid Deschenes <adeschen@hotmail.com>
2020-02-12 11:36:01 -0500

    Update getz() man page

--- R/getz.R
+++ R/getz.R
@@ -2,22 +2,33 @@
 #' 
 #' @description TODO
 #' 
-#' @param logr TODO
+#' @param logr A \code{vector} of \code{double} representing the log ratio
+#' values.
 #' 
 #' @param emfit An object of class \code{Mclust} providing a 
 #' mixture model estimation. 
 #' 
-#' @param zgroup TODO
+#' @param zgroup A \code{matrix} of \code{double}, used as 
+#' \code{integer}, with the number of rows corresponding to the current 
+#' number of clusters while the number of columns is 
+#' corresponding to the initial number of clusters. The presence of \code{1} 
+#' in position \emph{[i,k]} indicates that the initial \emph{i}th cluster 
+#' is now part of the new \emph{k}th cluster.
 #' 
 #' @param times A single \code{double} value specifying the number of 
 #' time the median of each segment is sampled in order to predict the cluster 
 #' assignment for the segment.
 #' 
-#' @return TODO
+#' @return A \code{matrix} of \code{double} containing the probability for each
+#' observation to belong to each class of the mixture model with the updated 
+#' number of clusters.
 #' 
 #' @examples
 #' 
 #' # TODO
+#' 
+#' ## Load 
+#' data(EMexample)
 #' 
 #' @author Alexander Krasnitz, Guoli Sun
 #' @importFrom stats predict
@@ -25,14 +36,20 @@
 getz <- function(logr, emfit, zgroup, times) { 
     
     ## Assign gz variable using mixture model when available
+    ## When available, the matrix containing the probability for each
+    ## observation to belong to each class is used (emfit$z)
     if (is.null(emfit$z)) {
         gz <- matrix(ncol = 1, data = rep(1, length(logr)))
     } else {
         gz <- predict(emfit, newdata=logr)$z
     }
     
+    
+    ## Identify positions where probability is not finite and assign zero
     isfin <- matrix(ncol = ncol(gz), nrow = nrow(gz), data = is.finite(gz))
     gz[!isfin] <- 0 #just being honest: we don't know how to assign these
+    
+    ## Sum the probability for each time the model is run
     gz <- matrix(ncol = ncol(gz),
         data = apply(gz, 2, cumsum)[seq(from = times, 
                                             to = nrow(gz), by = times),])
