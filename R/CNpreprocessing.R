59520a7c9315ca2b8345b9e0077385433487d256
belleau <pascal_belleau@hotmail.com>
2020-06-18 19:15:06 -0400

    Debug save clust TMP

--- R/CNpreprocessing.R
+++ R/CNpreprocessing.R
@@ -415,26 +415,28 @@ CNpreprocessing <- function(segall, ratall=NULL, idCol=NULL, startCol=NULL,
                                 BPPARAM=coreParam)
         
         #segall <- cbind(segall, do.call(rbind, processed))
-        segall <- cbind(segall, do.call(rbind, vapply(processed, FUN=function(x){return(list(x$seg))},FUN.VALUE = list(1))))
-        dimnames(segall)[[2]][(ncol(segall)-8):ncol(segall)] <-
-            c("segmedian", "segmad", "mediandev", "segerr", "centerz",
-                "marginalprob", "maxz", "maxzmean", "maxzsigma")
-        medCol <- "mediandev"
-        madCol <- "segmad"
-        errorCol <- "segerr"
-        
-        if(keepClust){
-            listClust <- list()
-            for(i in seq_len(length(processed))){
-                idValue <- unique(processed$idcol)
-                if(length(idValue) > 1){
-                    warning(paste0("Problem of idcol for element ", idValue[1]))
-                    idValue <- paste0("Ind", i)
-                }
-                listClust[[idValue]] <- processed[[i]]$resClust
-            }
-            saveRDS(listClust, "listClust.rds")
-        }
+        saveRDS(processed, "processed.rds")
+        segall <- processed
+        # segall <- cbind(segall, do.call(rbind, vapply(processed, FUN=function(x){return(list(x$seg))},FUN.VALUE = list(1))))
+        # dimnames(segall)[[2]][(ncol(segall)-8):ncol(segall)] <-
+        #     c("segmedian", "segmad", "mediandev", "segerr", "centerz",
+        #         "marginalprob", "maxz", "maxzmean", "maxzsigma")
+        # medCol <- "mediandev"
+        # madCol <- "segmad"
+        # errorCol <- "segerr"
+        # 
+        # if(keepClust){
+        #     listClust <- list()
+        #     for(i in seq_len(length(processed))){
+        #         idValue <- unique(processed$idcol)
+        #         if(length(idValue) > 1){
+        #             warning(paste0("Problem of idcol for element ", idValue[1]))
+        #             idValue <- paste0("Ind", i)
+        #         }
+        #         listClust[[idValue]] <- processed[[i]]$resClust
+        #     }
+        #     saveRDS(listClust, "listClust.rds")
+        # }
     }
     
     ## Use normal samples to extract extra information when available
